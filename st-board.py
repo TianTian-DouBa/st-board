9e82efe1f0b472c6b586d6abe19c0d35b425f1cd
TianTian-DouBa <fanxc1@hotmail.com>
2019-07-14 21:58:21 +0800

    update

--- st-board.py
+++ st-board.py
@@ -18,6 +18,7 @@ def get_stock_list(return_df = True):
     return df
 
 def load_stock_list():
+    """从保存的stock_list.csv文件中,读入返回DataFram"""
     file_name = "stock_list.csv"
     try:
         df = pd.read_csv(sub_path + '\\' + file_name)
@@ -26,6 +27,19 @@ def load_stock_list():
         df = None
     return df
 
+def get_daily_basic(return_df = True):
+    """获取TuShare最近交易日的每日指标，保存到daily_basic.csv文件,按需反馈DataFram
+    retrun_df:<bool> 是返回DataFrame数据，否返回None
+    """
+    file_name = "daily_basic.csv"
+    _trade_date = '20190712'
+    df = ts_pro.daily_basic(ts_code='', trade_date=_trade_date, fields='ts_code,trade_date,close,turnover_rate,turnover_rate_f,volume_ratio,pe,pe_ttm,pb,ps,ps_ttm,total_share,float_share,free_share,total_mv,circ_mv')
+    df.to_csv(sub_path + '\\' + file_name, encoding="utf-8")
+    if return_df != True:
+        df = None
+    return df
+
 if __name__ == "__main__":
     #df = get_stock_list()
-    df = load_stock_list()
+    #df = load_stock_list()
+    df = get_daily_basic()
