559264c45eb0ccc9bd0ce8252fd57e9bca9b22fd
TianTian-DouBa <fanxc1@hotmail.com>
2019-08-22 20:19:11 +0800

    update

--- st-board.py
+++ st-board.py
@@ -549,7 +549,7 @@ if __name__ == "__main__":
     #zs = que_index_daily(ts_code="000009.SH",start_date="20031231")
     #ttt = index.get_index_daily('399003.SZ',reload=False)
     download_cnfg_path = r".\data_csv\dowload_cnfg.csv"
-    #bulk_download(download_cnfg_path) 批量下载数据
+    #bulk_download(download_cnfg_path) #批量下载数据
     #ttt = ts_pro.index_daily(ts_code='801001.SI',start_date='20190601',end_date='20190731')
     #ttt = ts_pro.sw_daily(ts_code='950085.SH',start_date='20190601',end_date='20190731')
     Plot.try_plot()
