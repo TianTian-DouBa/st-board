0cd61e8557ca1024ee8c5c80cfddd3e07499b0f9
TianTian-DouBa <fanxc1@hotmail.com>
2019-10-13 21:19:40 +0800

    update

--- test.py
+++ test.py
@@ -1,30 +1,5 @@
-import tushare as ts
-import pandas as pd
-import numpy as np
-import os
-import time
-from datetime import datetime,timedelta
-from XF_common.XF_LOG_MANAGE import add_log, logable, log_print
-import matplotlib.pyplot as plt
-from pylab import mpl
-from st_board import Stock
-
-dates = pd.date_range('20190821',periods=6)
-df = pd.DataFrame(np.random.randn(6,4),index=dates,columns=['a','b','c','d'])
-dates1 = pd.date_range('20190821',periods=3)
-df1 = pd.DataFrame(np.random.randn(3,2),index=dates1,columns=['a','c'])
-df.loc[:,'e']=df1['a']
-combined = df
-#frames=[df,df1]
-#combined=pd.concat(frames)
-
-print('-----------------df-------------------')
-print(df)
-print('-----------------df1-------------------')
-print(df1)
-print('-----------------combined-------------------')
-print(combined)
-
+if __name__ == "__main__":
+    print("Hello Raspberrypi")
 
 
 
