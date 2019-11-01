da3addfca70a558c94d020469113532943d0609f
TianTian-DouBa <fanxc1@hotmail.com>
2019-11-01 09:41:18 +0800

    update

--- indicator.py
+++ indicator.py
@@ -40,17 +40,15 @@ class Indicator():
         self.source = None #<str>数据源，如'close_hfq'收盘后复权
         self.file_name = None
         self.file_path = None
-        print("[L40] 未完")
 
     def load_sources(self,nrows=None):
         """
-        调用st_board.load_source_df()来准备计算用原数据，并计算calc_res用的end_date_str;
+        调用st_board.load_source_df()来准备计算用原数据
         ts_code:<str> e.g. '000001.SH'
         nrows: <int> 指定读入最近n个周期的记录,None=全部
         retrun:<df> trade_date(index); close; high..., None if failed
         """
         df_source = load_source_df(ts_code=self.ts_code,source=self.source)
-        print('[L40] 是否需要end_date_str待考虑')
         return df_source
     
     def load_idt(self,nrows=None):
@@ -71,23 +69,29 @@ class Indicator():
         """
         调用self._calc_res()补完df_idt数据
         """
-        df_idt = self._calc_res()
-        if isinstance(df_idt, pd.DataFrame):
-            print("[L76] 拼接部分未完成")
+        df_append = self._calc_res()
+        if isinstance(df_append, pd.DataFrame):
+            if isinstance(self.df_idt, pd.DataFrame):
+                _frames = [df_append,self.df_idt]
+                df_idt = pd.concat(_frames,sort=False)
+            else:
+                df_idt = df_append
             if st_board.valid_file_path(self.file_path):
                 df_idt.to_csv(self.file_path)
+                log_args = [self.ts_code, self.file_path]
+                add_log(40,"[fn]Indicator.calc_idt() ts_code:{0[0]}, file_path:{0[1]}, saved", log_args)
             else:
-                log_args = [ts_code, file_path]
-                add_log(40,"[fn]Indicator.calc_idt() ts_code:{0[0]}, file_path:{0[1]}, invalid", log_args)
+                log_args = [self.ts_code, self.file_path]
+                add_log(20,"[fn]Indicator.calc_idt() ts_code:{0[0]}, file_path:{0[1]}, invalid", log_args)
             self.df_idt = df_idt
-        elif df_idt is None:
-            log_args = [ts_code]
-            add_log(30,"[fn]Indicator.calc_idt() ts_code:{0[0]}, return None to .df_idt", log_args)
-            self.df_idt = None
+            log_args = [self.ts_code, self.file_name[:-4], len(df_idt)]
+            add_log(40,"[fn]Indicator.calc_idt() ts_code:{0[0]}, {0[1]} updated; items:{0[2]}", log_args)
+        elif df_append is None:
+            pass #keep self.df_idt as it is
         else:
-            log_args = [ts_code, type(df_idt)]
-            add_log(10,"[fn]Indicator.calc_idt() ts_code:{0[0]}, type(df_idt):{0[1]}, unknown problem", log_args)
-            self.df_idt = None
+            log_args = [self.ts_code, type(df_idt)]
+            add_log(10,"[fn]Indicator.calc_idt() ts_code:{0[0]}, type(df_append):{0[1]}, unknown problem", log_args)
+            pass #keep self.df_idt as it is
 
 
     def _calc_res(self):
@@ -125,7 +129,7 @@ class Ma(Indicator):
         self.period = period
         #print("[L97] 补period类型异常")
         self.source = 'close_hfq'
-        self.file_name = 'idt_' + ts_code + '_' + self.idt_type + '_' + subtype + str(period) + '.csv'
+        self.file_name = 'idt_' + ts_code + '_' + self.source + '_' + self.idt_type + '_' + subtype + str(period) + '.csv'
         self.file_path = sub_path + sub_idt + '\\' + self.file_name
         self.subtype = subtype
 
@@ -150,9 +154,6 @@ class Ma(Indicator):
                 return
             elif idt_head_in_source > 0:
                 df_source.drop(df_source.index[idt_head_in_source + period - 1:],inplace=True)
-                print("[L152]--------------df_source, begin----------------")
-                print(df_source)
-                print("[L152]--------------df_source, end----------------")
                 values = []
                 rvs_rslt = []
                 try:
@@ -188,16 +189,12 @@ class Ma(Indicator):
         idt_column_name = 'MA' + str(period)
         data = {idt_column_name:rslt}
         df_idt_append = pd.DataFrame(data,index=index_source)
-        print("[L187]--------------df_idt_append, begin----------------")
-        print(df_idt_append)
-        print("[L189]--------------df_idt_append, end----------------")
         return df_idt_append
 
 if __name__ == '__main__':
     start_time = datetime.now()
     #raw_data_init()
     global raw_data
-    print("[L185]: raw_data:{}".format(raw_data))
     #------------------test---------------------
     ma10 = Ma(ts_code='000002.SZ',period=10)
     ma10.calc_idt()
